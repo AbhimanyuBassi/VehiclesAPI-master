0fefa606f6fabe2e5a6b8dbac68801d93ddcfb47
Abhimanyu <abhimanyu.bassi@gmail.com>
2019-10-04 01:19:08 +0800

    Pricing Service API is converted to a microservice

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,37 +1,63 @@
-
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
 
 	@Autowired
-	private MockMvc mvc;
+	PriceRepository priceRepository;
+
+	@Autowired
+	MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 	@Test
-	public void testGetPrice() throws Exception{
-		mvc.perform(get("/services/price?vehicleId=1"))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.price").exists())
-				.andExpect(jsonPath("$.currency").value("USD"))
-				.andExpect(jsonPath("$.vehicleId").value(1));
+	public void findPrice() throws Exception {
+		mvc.perform(
+				get("/prices/1")
+						.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk());
 	}
+
+	@Test
+	public void findAll(){
+		List<Price> prices = (List<Price>) priceRepository.findAll();
+		assertEquals(4, prices.size());
+		assertEquals(1, prices.get(0).getVehicleId().intValue());
+		assertEquals(2, prices.get(1).getVehicleId().intValue());
+	}
+
+	@Test
+	public void findByVehicleId(){
+
+		Optional<Price> optionalPrice = priceRepository.findById(1L);
+		assertTrue(optionalPrice.isPresent());
+	}
+
 
 }
